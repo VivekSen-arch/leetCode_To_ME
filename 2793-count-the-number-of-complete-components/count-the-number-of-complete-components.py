47ce73c7ad81cb897f9615611de6d030c7b2a35f
Vivek Sen <senvivek561@gmail.com>
2026-07-11 08:49:53 +0530

    Time: 38 ms (68.17%) | Memory: 21.1 MB (27.33%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        visited = [False] * n
+        ans = 0
+        def dfs(node):
+            visited[node] = True
+            component.append(node)
+            for nei in graph[node]:
+                if not visited[nei]:
+                    dfs(nei)
+        for i in range(n):
+            if not visited[i]:
+                component = []
+                dfs(i)
+                size = len(component)
+                edge_count = 0
+                for node in component:
+                    edge_count += len(graph[node])
+                edge_count //= 2
+                if edge_count == size * (size - 1) // 2:
+                    ans += 1
+        return ans
