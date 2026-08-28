bd47a9f33a675342eba51f658d1412d06149d8c5
Vivek Sen <senvivek561@gmail.com>
2026-08-28 19:36:50 +0530

    Time: 36 ms (43.94%) | Memory: 19.4 MB (68.18%) - LeetSync

---
+++ 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.py
@@ -0,0 +1,97 @@
+class Solution:
+    def lexPalindromicPermutation(self, s: str, target: str) -> str:
+        n = len(s)
+
+        cnt = [0] * 26
+
+        for ch in s:
+            cnt[ord(ch) - ord('a')] += 1
+
+        # Check if palindrome is possible
+        odd = 0
+        middle = ""
+
+        for i in range(26):
+            if cnt[i] % 2 == 1:
+                odd += 1
+                middle = chr(ord('a') + i)
+
+        if odd > 1:
+            return ""
+
+        # Characters for left half
+        half = [0] * 26
+
+        for i in range(26):
+            half[i] = cnt[i] // 2
+
+        m = n // 2
+
+        def make_pal(left):
+            left = ''.join(left)
+
+            if n % 2 == 1:
+                return left + middle + left[::-1]
+
+            return left + left[::-1]
+
+        # First check if target's left half itself is possible
+        used = [0] * 26
+        possible = True
+
+        for i in range(m):
+            c = ord(target[i]) - ord('a')
+            used[c] += 1
+
+            if used[c] > half[c]:
+                possible = False
+                break
+
+        if possible:
+            candidate = make_pal(list(target[:m]))
+
+            if candidate > target:
+                return candidate
+
+        # Find the smallest left half greater than target's left half
+        for pos in range(m - 1, -1, -1):
+
+            used = [0] * 26
+            possible = True
+
+            for j in range(pos):
+                c = ord(target[j]) - ord('a')
+                used[c] += 1
+
+                if used[c] > half[c]:
+                    possible = False
+                    break
+
+            if not possible:
+                continue
+
+            cur = ord(target[pos]) - ord('a')
+
+            for c in range(cur + 1, 26):
+
+                if used[c] >= half[c]:
+                    continue
+
+                left = list(target[:pos])
+                left.append(chr(ord('a') + c))
+
+                rem = half[:]
+
+                for x in range(26):
+                    rem[x] -= used[x]
+
+                rem[c] -= 1
+
+                for x in range(26):
+                    left.extend(
+                        [chr(ord('a') + x)] * rem[x]
+                    )
+
+                return make_pal(left)
+
+        return ""
